8f1a694d40e4d0c6facffcff3d73dd73c8d7a030
gregorybi <gregorykymi5@gmail.com>
2025-04-12 13:11:46 +0300

    Update analyzer

--- resume_analyzer.py
+++ resume_analyzer.py
@@ -7,7 +7,7 @@ nltk.download('punkt')
 
 # CV text
 resume_text = """
-George Panagiotopoulos is a junior machine learning engineer with a background in SVM-based classification models.
+George Athanasiou is a junior machine learning engineer with a background in SVM-based classification models.
 He gratuated recently and completed his research thesis using support vector machines for text classification.
 Skilled in Python, Scikit-learn, K-means clustering, data preprocessing, and model evaluation.
 He is a team player, fast learner and problem solver 
@@ -21,19 +21,22 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_resume(text):
     
-    summary = summarizer(text, max_length=100, min_length=30, do_sample=False)
+    summary = summarizer(text, max_length=100, min_length=60, do_sample=False)
     return summary[0]['summary_text']
 
 # Find skills
 tech_keywords = ["Python", "Scikit-learn", "PyTorch", "TensorFlow", "data preprocessing",
-                 "SVM", "transformer", "BERT", "NLP", "machine learning", "model evaluation"]
+                 "SVM", "transformer", "BERT", "NLP", "machine learning", "model evaluation"
+                 , "large language models"]
 
 soft_keywords = ["team", "communication", "problem solver", "adapt", "fast learner", "collaborate", 
                  "attention to detail"]
 
+
+# return first 5 skills from the resume
 def extract_skills(text, keywords):
     found = [word for word in keywords if re.search(rf"\b{word}\b", text, re.IGNORECASE)]
-    return found
+    return found[:5]
     
 
 # Improvement suggestions
