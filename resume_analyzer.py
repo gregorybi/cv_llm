72ddfac057608df14d9e41af84bd6e871ed3f86f
gregorybi <gregorykymi5@gmail.com>
2025-04-12 12:43:25 +0300

    Update analyzer

--- resume_analyzer.py
+++ resume_analyzer.py
@@ -10,7 +10,7 @@ resume_text = """
 George Panagiotopoulos is a junior machine learning engineer with a background in SVM-based classification models.
 He completed his research thesis using support vector machines for text classification.
 Skilled in Python, Scikit-learn, data preprocessing, and model evaluation.
-A team player and fast learner with strong communication skills. 
+A team player and fast learner with strong communication skills and keen atention to detail. 
 Currently studying large language models and transformer architectures.
 """
 
@@ -27,11 +27,13 @@ def summarize_resume(text):
 tech_keywords = ["Python", "Scikit-learn", "PyTorch", "TensorFlow", "data preprocessing",
                  "SVM", "transformer", "BERT", "NLP", "machine learning", "model evaluation"]
 
-soft_keywords = ["team", "communication", "problem", "adapt", "fast learner", "collaborate"]
+soft_keywords = ["team", "communication", "problem", "adapt", "fast learner", "collaborate", 
+                 "attention to detail"]
 
 def extract_skills(text, keywords):
     found = [word for word in keywords if re.search(rf"\b{word}\b", text, re.IGNORECASE)]
-    return list(set(found))[:5]
+    return found
+    # return list(set(found))[:5]
 
 # Improvement suggestions
 def suggest_improvements(text):
@@ -50,11 +52,13 @@ if __name__ == "__main__":
     print(summarize_resume(resume_text))
 
     print("\n Technical Skills Found:")
-    print(", ".join(extract_skills(resume_text, tech_keywords)))
+    print (*extract_skills(resume_text, tech_keywords), sep=', ')
+    # print(", ".join(extract_skills(resume_text, tech_keywords)))
 
     print("\n Soft Skills Found:")
-    print(", ".join(extract_skills(resume_text, soft_keywords)))
+    print (*extract_skills(resume_text, soft_keywords), sep=', ')
+    # print(", ".join(extract_skills(resume_text, soft_keywords)))
 
     print("\n Suggestions:")
-    for s in suggest_improvements(resume_text):
-        print(s)
+    for suggestion in suggest_improvements(resume_text):
+        print(suggestion)
